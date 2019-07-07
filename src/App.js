99f1124000dde318f735a2c57e952949dc3b5f94
Prafull <prafullkaranje@gmail.com>
2019-07-07 14:42:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container--1">
+        <div className="key key--lock"> lock </div>
+        <div className="key key--divide"> / </div>
+        <div className="key key--multi">*</div>
+
+        <div className="key">1</div>
+        <div className="key">
+          <p className="sub-item">2</p>
+        </div>
+        <div className="key">3</div>
+        <div className="key">4</div>
+        <div className="key key--5">5</div>
+        <div className="key">6</div>
+        <div className="key">7</div>
+        <div className="key">8</div>
+        <div className="key">9</div>
+        <div className="key key--zero">0</div>
+
+
+        <div className="key key--add">+</div>
+        <div className="key key--minus">-</div>
+
+        <div className="key key--dot"> . </div>
+
+        <div className="key key--enter"> enter </div>
+      </div>
     </div>
   );
 }
